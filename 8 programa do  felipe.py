7cf5e19f40899ae23c2167d74ff22611f4dbad50
Cleyson <cleyson.bonfim@gmail.com>
2020-07-28 14:23:14 -0300

    7-8 Programa do Felipe

---
+++ 8 programa do  felipe.py
@@ -0,0 +1,12 @@
+import turtle
+t = turtle.Pen()
+turtle.bgcolor("pink")
+# Você pode escolher entre 2 e 6 lados para obter algumas formas interessantes
+sides = 6
+colors = ["red", "yellow", "blue", "orange", "green", "purple"]
+for x in range(360):
+    t.pencolor(colors[x%sides])
+    t.forward(x * 3/sides + x)
+    t.left(360/sides + 1)
+    t.width(x*sides/200)
+    
