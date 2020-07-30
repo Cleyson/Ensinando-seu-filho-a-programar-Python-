f766579ff974eea0e9273fcb8398886bbd2df705
Cleyson <cleyson.bonfim@gmail.com>
2020-07-30 16:31:18 -0300

    9 programa do Felipe

---
+++ 9 programa do felipe.py
@@ -0,0 +1,7 @@
+my_name = "Felipe"
+my_age = 8
+your_name = input("What is your name? ")
+your_age = input("What old are you? ")
+print("My name is",my_name, ", and I am",my_age, "years old.")
+print("your name is",your_name, ", and you are",your_age, ".")
+print("Thank you for buying my book,",your_name, "!")
