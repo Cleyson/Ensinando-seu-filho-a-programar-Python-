d9f7c2f6e5a44e2d43af1762a46a2176e86116f0
Cleyson <cleyson.bonfim@gmail.com>
2020-08-04 15:24:48 -0300

    11 programa do felipe

---
+++ 11 programa do felipe.py
@@ -0,0 +1,26 @@
+import pygame    # Configuração
+pygame.init()
+screen = pygame.display.set_mode([800,600])
+pygame.display.set_caption("Smiley pong")
+keepGoing = True
+pic = pygame.image.load("CrazySmile.bmp")
+colorkey = pic.get_at((0,0))
+pic.set_colorkey(colorkey)
+picx = 0
+picy = 0
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+timer = pygame.time.Clock()
+speedx = 5
+speedy = 5
+paddlew = 200
+paddleh = 25
+paddlex = 300
+paddley = 550
+picw = 100
+pich = 100
+points = 0
+lives = 5
+font = pygame.font.SysFont("Times", 24)
+
+                          
