c40e99ce1a48cc260b45fcca2ffcd2f9e3de0b92
Cleyson <cleyson.bonfim@gmail.com>
2020-09-04 17:43:22 -0300

    14 - programa do felipe

---
+++ 14 programa do felipe.py
@@ -0,0 +1,23 @@
+# SAyMyName.py - exibe uma tela cheia do nome do usuario
+
+# Pede o nome do osuario
+name = input("What is your name? ")
+
+# Exibe o nome do osuario, 100 vezes
+for x in range(1000):
+    # Exibe o nome do osuario, seguido de um espaço, e nao de
+    # uma quebra de linha
+    print(name, end = " ")
+    
+
+
+
+
+
+
+
+
+
+
+
+
